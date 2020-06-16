8f3aaebe4b5bb9ca351a18cc5a6456f3b171ccba
Inseong-So <goflvhxj2547@gmail.com>
2020-06-16 20:08:56 +0900

    Lotto!

---
+++ javaBase/src/test/java/com/sis/lotto/LottoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sis.lotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoApplicationTests
+{
+    
+    @Test
+    void contextLoads()
+    {
+    }
+    
+}
